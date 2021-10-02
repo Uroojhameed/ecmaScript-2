3feab5f3968a4863e91aa880b507a4f952d3f8cf
Uroojhameed <81069857+Uroojhameed@users.noreply.github.com>
2021-10-02 20:19:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,66 @@
+// assignment-1
+// write a function which returns array. call the function and destructure array values and print the value
+
+
+// function userName() {
+//     return ['Faiza','Aziz',2] 
+// }
+// console.log(userName())
+
+// let [firstName,lastName,degree] = userName()
+// console.log(degree)
+
+
+// Make a car object with model, year, color. display all the values by destructuring
+
+
+
+// let car = {
+// model : "ak5j", 
+// color : "black",
+// yera : 2020,
+// }
+// let {model:first, color:second, yera:yera} = car
+// console.log(first)
+
+
+
+// use three variable name, greeting and display value using template literals
+
+// let name  = "hello"
+// let name2 = "hyee"
+// let name3 = "asalam u alikum"
+
+// // alert("Welcome" + name + name2 + name3)
+
+// alert(`Welcome ${name} ${name2} ${name3}`) templat litrals
+
+
+
+// use array [1,2,3,4,5,6,7,8,9,10] assign first three values in seperate variables and all remaining in one element as an array
+
+
+// const arr = [1,2,3,4,5,6,7,8,9,10]
+// let [pehlewale,sec,th] = arr
+// console.log(pehlewale)
+// console.log(sec)
+// console.log(th)
+
+// let remain = arr.slice(3,10);
+// console.log(remain)  
+
+
+// if car speed is less than 80 display "car speed is slow", if greater than 80 but less than 120 than display 
+// " car speed is moderate" and if greater than 120 display "car speed is fast"
+
+
+
+// let car = 200;
+// if (car < 80){
+//     alert("car speed is slow")
+// }else if (car > 80 && car <  120){
+//     alert("car speed is moderate")
+
+// }else if(car > 120){
+//     alert("car speed is fast")
+// }
